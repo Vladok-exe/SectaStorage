4972939605749b625bde5504c8e312caf17650e6
Vladok-exe <hrapynenko@gmail.com>
2020-02-24 20:42:26 +0300

    2

--- Лабараторная/Лабараторная/Sobaka.cs
+++ Лабараторная/Лабараторная/Sobaka.cs
@@ -8,5 +8,21 @@ namespace Лабараторная
 {
     class Sobaka
     {
+        private string klichka { get; set; }
+        private string poroda { get; set; }
+        private float vozrast { get; set; }
+        public void vivod()
+        {
+            Console.WriteLine("Кличка:" + klichka + " " + "Порода:" + poroda + " " + "Возраст:" + vozrast + " ");
+        }
+        public void write()
+        {
+            Console.WriteLine("Требуется ввести кличку собаки: ");
+            klichka = Console.ReadLine();
+            Console.WriteLine("Требуется ввести породу собаки: ");
+            poroda = Console.ReadLine();
+            Console.WriteLine("Требуется ввести возраст собаки: ");
+            vozrast = int.Parse(Console.ReadLine());
+        }
     }
 }
